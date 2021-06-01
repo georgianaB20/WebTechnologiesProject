413aa4cd01c1e22ce69eab09308793578f9d6363
@ miky.miky1999.miky@gmail.com <61892564+mikyqwe@users.noreply.github.com>
2021-06-01 18:11:27 +0300

    front comment section cu colapsare poza js

--- tw_proiect/frontend/js/reteta.js
+++ tw_proiect/frontend/js/reteta.js
@@ -39,5 +39,21 @@ xhttp.onreadystatechange = function() {
         element.innerHTML = `<img class="img" src='${recipe.picture}' alt="Poza a retetei 1">`
     }
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 xhttp.open("GET", "http://localhost:5000/recipe?id=" + id, true);
 xhttp.send();
