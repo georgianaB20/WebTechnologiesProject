71800438519f423a9423bb5437fac33dd984e1dd
Cernat Monica Roxana <cernatmonicaroxana@gmail.com>
2021-06-08 03:58:42 +0300

    revocat dreptul de a adauga retete

--- tw_proiect/frontend/js/admins_page.js
+++ tw_proiect/frontend/js/admins_page.js
@@ -1,6 +1,8 @@
 document.getElementById("ofera_comentarii").addEventListener("click", add_right_comment);
 document.getElementById("restrictioneaza_comentarii").addEventListener("click", remove_right_to_comment);
+
 document.getElementById("ofera_retete").addEventListener("click", add_right_recipes);
+document.getElementById("restrictioneaza_retete").addEventListener("click", remove_right_recipe);
 
 
 function add_right_comment(event) {
@@ -91,6 +93,40 @@ function add_right_recipes(event) {
     const xhttp = new XMLHttpRequest()
 
     xhttp.open('PUT', 'http://localhost:5000/grant?type=post');
+
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.setRequestHeader("Accept", "application/json");
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhttp.setRequestHeader("Authorization", localStorage.getItem('AuthorizationToken'));
+
+    xhttp.onload = function () {
+        let res = JSON.parse(xhttp.response)
+        if (xhttp.status !== 200) {
+            alert(res.message);
+        } else {
+            alert(res.message)
+        }
+    }
+    xhttp.send(JSON.stringify(body))
+}
+
+function remove_right_recipe(event) {
+    event.preventDefault();
+    let user = document.getElementById("username_retete").value;
+    let email_user = document.getElementById('email_retete').value;
+    let parola_admin = document.getElementById("parola_admin_retete").value
+
+    let body = {
+        "username": `${user}`,
+        "email_user": `${email_user}`,
+        "parola_admin": `${parola_admin}`
+    }
+
+    console.log(body)
+
+    const xhttp = new XMLHttpRequest()
+
+    xhttp.open('PUT', 'http://localhost:5000/restrict?type=post');
 
     xhttp.setRequestHeader("Content-Type", "application/json");
     xhttp.setRequestHeader("Accept", "application/json");
